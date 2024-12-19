abdadf1c29817488e9c8593e6391484e75317c16
aleyufra <alejandromaryufra@gmail.com>
2024-12-19 18:42:11 -0300

    add: estructura inicial

---
+++ src/main/java/com/threequeens/ventacalzados/VentacalzadosApplication.java
@@ -0,0 +1,13 @@
+package com.threequeens.ventacalzados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentacalzadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VentacalzadosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/threequeens/ventacalzados/VentacalzadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threequeens.ventacalzados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentacalzadosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
